7501298e3a4c60db2b51f49b3c7b770d69e32dd3
Vincenzo Iorio <ioriovincenzo.msft@outlook.com>
2025-08-14 18:09:59 +0200

    first commit

---
+++ VirtualAssistant/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VirtualAssistant
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
